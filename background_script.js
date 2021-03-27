c7e44efe5b0ac688df6d459201976c1cb55d3be8
Thejus Paul <thejuspaul@pm.me>
2021-03-27 11:37:28 +0530

    docs: minor documentation changes

--- background_script.js
+++ background_script.js
@@ -1,13 +1,15 @@
 let dramaSpyList = {};
+
+// Uses cache to prevent re-fetching
 async function notify(message) {
-    if(dramaSpyList.length>0) {
+    if(dramaSpyList.length > 0) {
         return dramaSpyList;
     } else {
-        fetchCount++;
         const response = await fetch('https://sponge-imminent-text.glitch.me/dramaspy/list');
         const parseResponse = await response.json();
         dramaSpyList = parseResponse.data;
         return parseResponse.data;
     }
 }
+
 browser.runtime.onMessage.addListener(notify);
