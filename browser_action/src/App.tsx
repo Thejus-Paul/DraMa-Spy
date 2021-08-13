3728cd2dc52f6f8c8276a01fa50f2be93de478f9
Thejus Paul <32426070+Thejus-Paul@users.noreply.github.com>
2021-08-13 07:51:32 +0530

    fix: background image loading delay
    chore: refactored the import statements

--- browser_action/src/App.tsx
+++ browser_action/src/App.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
-
+import { useState, useEffect } from 'react';
 import './App.css';
 
 interface dramaItems {
