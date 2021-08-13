c94a9f7bd610da1438df959f5303a9b7b98398ae
Thejus Paul <32426070+Thejus-Paul@users.noreply.github.com>
2021-08-13 18:10:25 +0530

    perf: cache data in localStorage

--- browser_action/src/App.tsx
+++ browser_action/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import CryptoJS from 'crypto-js';
 import './App.css';
 
 interface dramaItems {
@@ -7,24 +8,44 @@ interface dramaItems {
 }
 
 function App() {
-  const [watchedList, setWatchedList] = useState<Array<dramaItems>>([]);
-  const [searchStr, setSearchStr] = useState<string>("");
-  const [searchResults, setSearchResults] = useState<Array<dramaItems>>([]);
+  const hash = (data: string) => CryptoJS.SHA3(data).toString();
+  const verify = (hash1: string, hash2: string) => (hash1 === hash2) ? true : false;
+  const encrypt = (data: Array<dramaItems>,key: string) => CryptoJS.AES.encrypt(JSON.stringify(data),key).toString()
+  const decrypt = (cipher: string,key: string) => JSON.parse(CryptoJS.AES.decrypt(cipher,key).toString(CryptoJS.enc.Utf8))
+  
+  const [watchedList, setWatchedList] = useState<Array<dramaItems>>([])
+  const [searchStr, setSearchStr] = useState<string>("")
+  const [searchResults, setSearchResults] = useState<Array<dramaItems>>([])
+  const localHash: string = String(localStorage.getItem("hashedList"))
+
+  useEffect(() => {
+    if(localStorage.getItem("watchedList")) 
+      setWatchedList(decrypt(String(localStorage.getItem("watchedList")),"test"))
+  },[])
 
   useEffect(() => {
     fetch('https://sponge-imminent-text.glitch.me/dramaspy/list')
 		.then(response => response.json())
-		.then(response => setWatchedList(response.data));
-  },[]);
+    .then(response => {
+      let currentHash = hash(JSON.stringify(response.data))
+      if(verify(currentHash,localHash)) console.log("Same Data")
+      else {
+        setWatchedList(response.data)
+        window.localStorage.setItem("watchedList",encrypt(response.data, "test").toString())
+        window.localStorage.setItem("hashedList",currentHash)
+        console.log("Data Set")
+      }
+    })
+  }, [localHash]);
 
   const handleInput = (value: string) => {
     setSearchStr(value);
     setSearchResults(watchedList.filter(
       (item) => 
       item['name'].toLowerCase()
-      .includes(value.toLowerCase())));
-    }
-    console.log(searchStr,searchResults)
+      .includes(value.toLowerCase())))
+  }
+  
   return (
     <div className="App"> 
       <div className="main">
