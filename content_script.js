85cdda4ab222f83c91c0e370475d68cada94d2fe
Thejus Paul <32426070+Thejus-Paul@users.noreply.github.com>
2021-08-15 20:44:50 +0530

    WIP: retrieve drama info

--- content_script.js
+++ content_script.js
@@ -14,16 +14,42 @@
 				browser.runtime.sendMessage(JSON.stringify({"command":"update","dramaList":list}));
 			}
 		});
+	const getDataURL = (img) => {
+		const canvas = document.createElement('canvas');
+		const ctx = canvas.getContext('2d');
+		canvas.width = img.width;
+		canvas.height = img.height;
+		ctx.drawImage(img, 0, 0);
+		return canvas.toDataURL('image/jpeg');
+	}
+	const getDramaSummary = () => {
+		const summaryElements = document.querySelector(".bigChar").parentNode.children
+		const getData = (item) => summaryElements.item(item).innerText.split(":")[1];
+		let drama = {}
+		drama.name = summaryElements.item(0).innerText
+		drama.otherNames = getData(1).split(";").map(name => name.trim())
+		drama.country = getData(2).trim()
+		drama.genre = getData(3).split(",").map(genre => genre.trim())
+		drama.aired = getData(4).trim()
+		const [status,views] = summaryElements.item(5).innerText.split(" ").filter(item => item.split(":")[1])
+		drama.status = status.split(":")[1].trim()
+		drama.views = views.split(":")[1].trim()
+		drama.summary = summaryElements.item(7).innerText
+		drama.image = getDataURL(document.querySelectorAll(".barContent img")[2])
+		drama.latestEpisode = Number(document.querySelector(".episodeSub").innerText.split(" ").pop())	
+		Object.freeze(drama);
+		return drama; 
+	}
 
 	function loadDraMaSpy(dramaList) {
 		// On a drama's episode page
 		if(window.location.hostname === "kissorg.net" && window.location.pathname.startsWith("/p/")) {
-			let tmp = document.querySelector("#navsubbar").children[0].innerText;
-			let drama = tmp.split(" ").slice(1,-1).join(" ");
-			let episodeSelector = document.getElementById("selectEpisode");
-			let currentEpisode = episodeSelector[episodeSelector.selectedIndex].value;
+			const tmp = document.querySelector("#navsubbar").children[0].innerText;
+			const drama = tmp.split(" ").slice(1,-1).join(" ");
+			const episodeSelector = document.getElementById("selectEpisode");
+			const currentEpisode = episodeSelector[episodeSelector.selectedIndex].value;
 			currentEpisode = Number(currentEpisode.split("?")[0].split("-")[1]);
-			let didFind = Boolean(dramaList.find(item => item.name == drama));
+			const didFind = Boolean(dramaList.find(item => item.name == drama));
 			if(didFind) {
 				dramaList.find(item => item.name == drama).lastWatched = currentEpisode;
 				return dramaList;
@@ -35,25 +61,25 @@
 
 		// On the drama or movie description page
 		if(window.location.pathname.startsWith("/Drama/")) {
-			let drama = window.location.pathname.split("/")[2].split("-").join(" ")
-			let contentType = document.getElementsByClassName("dotUnder");
+			const drama = window.location.pathname.split("/")[2].split("-").join(" ")
+			const contentType = document.getElementsByClassName("dotUnder");
 			if((window.location.hostname === "kissasian.li") && (contentType.length > 0)) {
-				let genre = [...contentType];
-				let isMovie = Boolean(genre.find(item => item.text === "Movie"));
-				let message = isMovie ? ("Movie: " + drama) : ("Drama: " + drama);
-				console.log(message);
-				let didFind = Boolean(dramaList.find(item => item.name == drama));
+				const genre = [...contentType];
+				const isMovie = Boolean(genre.find(item => item.text === "Movie"));
+				const message = isMovie ? ("Movie: " + drama) : ("Drama: " + drama);
+				const didFind = Boolean(dramaList.find(item => item.name == drama));
 				if(didFind) {
-					let currentDrama = dramaList.find(item => item.name == drama)
+					const currentDrama = dramaList.find(item => item.name == drama)
 					console.log("You've watched this drama till episode",currentDrama.lastWatched);
-					let totalEpisodes = document.getElementsByClassName('episodeSub').length;
-					let indexOfLastWatchedEpisode = totalEpisodes - currentDrama.lastWatched;
+					const totalEpisodes = document.getElementsByClassName('episodeSub').length;
+					const indexOfLastWatchedEpisode = totalEpisodes - currentDrama.lastWatched;
 					document.getElementsByClassName('episodeSub')[indexOfLastWatchedEpisode].children.item(0).text += " 👀";
 					for(let i = (totalEpisodes - currentDrama.lastWatched); i <= totalEpisodes; i++) {
 						document.getElementsByClassName('episodeSub')[i].children.item(0).style.color = "lightgreen";
 					}
 				}
 			}
+			getDramaSummary()
 		}
 	}
 })();
